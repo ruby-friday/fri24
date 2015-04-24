bacc1a21301f1e1dda5181206004de831bd2b1ab
James Muranga <jmured@gmail.com>
2015-04-24 16:53:13 +0300

    can create the participant then list

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
+require_relative 'participant'
 
 get '/' do
+  @participants = Participant.all
   erb :index
 end
 
 post '/add' do
   puts @params
+  p = Participant.new
+  p.first_name = @params['first_name']
+  p.last_name = @params['last_name']
+  p.gender = @params['gender']
+  p.save
   redirect to('/')
 end
